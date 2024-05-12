498c8372cdcfd2ac5833276e8d5b86d0ed28ce68
firdous alam <firdous.alam2058@gmail.com>
2024-05-12 13:41:09 +0530

    port update

--- backendexpress/index.js
+++ backendexpress/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 4000
+const port = 5000
 
 app.get('/uadaypad', (req, res) => {
-  res.send('Hello UdayPad!')
+  res.send('Hello UdayPad! ')
 })
 
 app.listen(port, () => {
