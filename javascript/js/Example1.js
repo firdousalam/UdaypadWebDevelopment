491a78fd6c3d920cff619a64c99aac4b30d9eb6e
firdous alam <firdous.alam2058@gmail.com>
2024-05-19 13:32:44 +0530

    19th May class changes

--- javascript/js/Example1.js
+++ javascript/js/Example1.js
@@ -4,11 +4,10 @@ let y = 20;
 let jjjjjj; // declaring variable
 jjjjjj = 30 // assign value to variable
 let z = jjjjjj;
-console.log("HELLO WORLD","sum ="+(x+y+z));
+console.log("HELLO WORLD","sum ="+(x+y));
 var varx = 10;
 var varx = 20;
 let vary = "gjhgjgg";
-
 const varz = {"gg" : 100};
 varz.gg = "testt";
 console.log(" const object",varz);
