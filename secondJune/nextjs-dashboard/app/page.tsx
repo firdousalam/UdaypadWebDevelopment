0cdb543f429590f87304b8abb257c21ca978f6a9
firdous alam <firdous.alam2058@gmail.com>
2024-06-15 19:38:33 +0530

    Code updare 15th June

--- secondJune/nextjs-dashboard/app/page.tsx
+++ secondJune/nextjs-dashboard/app/page.tsx
@@ -15,13 +15,13 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Welcome to Acme nnn.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
             , brought to you by Vercel. 
           </p>
-          <h1 className="text-blue-500">I'm blue!</h1>
+          <h1 className="text-blue-500">I am blue!</h1>
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
